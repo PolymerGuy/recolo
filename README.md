6fd6d18595d65a4d1ff5c3ca0a05a59b6b8007a7
Sindre Nordmark Olufsen <sindre.n.olufsen@ntnu.no>
2021-03-01 08:38:24 +0100

    Updated structure

---
+++ README.md
@@ -0,0 +1,9 @@
+# Recon
+Tools for load reconstruction using the virtual fields method.
+
+## General info
+Rough project draft
+
+## Setup
+Tested using Python 3.7
+
--- recon/diff_tools.py
+++ recon/diff_tools.py
@@ -1,21 +1,21 @@
 import numpy as np
-def dF_complex_x(func,x,y,step):
+def dF_complex_x(func,x,y,step=1.e-6):
     '''Complex step aproximation'''
     return np.imag(func(x + 1.j*step,y))/step
 
-def ddF_complex_x(func,x,y,step):
+def ddF_complex_x(func,x,y,step=1.e-6):
     '''Complex step aproximation of second derivative'''
     return (2./step**2.)*(func(x,y)-np.real(func(x + 1.j*step,y)))
 
-def ddF_complex_y(func,x,y,step):
+def ddF_complex_y(func,x,y,step=1.e-6):
     '''Complex step aproximation of second derivative'''
     return (2./step**2.)*(func(x,y)-np.real(func(x,y + 1.j*step)))
 
 
-def dF_complex_y(func,x,y,step):
+def dF_complex_y(func,x,y,step=1.e-6):
     '''Complex step aproximation'''
     return np.imag(func(x,y + 1.j*step))/step
 
-def ddF_complex_xy(func,x,y,step):
+def ddF_complex_xy(func,x,y,step=1.e-6):
     '''Complex step aproximation of derivative'''
     return (1./step**2.) * (func(x,y) -np.real(func(x + 1.j*step ,y + 1.j*step)))-0.5*ddF_complex_x(func,x,y,step)-0.5*ddF_complex_y(func,x,y,step)
