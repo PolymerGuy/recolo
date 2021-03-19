9091120ab6db2d4d0fe10523c437b4cb6d162b9e
Sindre Nordmark Olufsen <sindre.n.olufsen@ntnu.no>
2021-03-19 10:43:58 +0100

    Renaming

--- recon/__init__.py
+++ recon/__init__.py
@@ -1,5 +1,5 @@
 from . import virtual_fields
 from .solver import *
 from .data_import import *
-from recon.framestack import *
+from recon.fieldstack import *
 from .plate import calculate_plate_stiffness
