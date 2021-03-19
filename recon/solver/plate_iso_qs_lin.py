42af0cf3f339b729a8934aa33382ccd4a695cccb
Sindre Nordmark Olufsen <sindre.n.olufsen@ntnu.no>
2021-03-19 09:47:35 +0100

    Renaming and removing internal energy as output

--- recon/solver/plate_iso_qs_lin.py
+++ recon/solver/plate_iso_qs_lin.py
@@ -33,23 +33,23 @@ def neighbour_map(iprw, size_with_pads_y, size_with_pads_x):
     return aux_neighbour + stencil
 
 
-def plate_iso_qs_lin(fields,  plate,vfields):
+def plate_iso_qs_lin(fields, plate, virtual_field):
 
-    A11 = convolve2d(fields.curv_xx, vfields.okxxfield, mode="valid") + convolve2d(fields.curv_yy, vfields.okyyfield,
-                                                                                   mode="valid") + 2. * convolve2d(
-        fields.curv_xy, vfields.okxyfield, mode="valid")
+    A11 = convolve2d(fields.curv_xx, virtual_field.okxxfield, mode="valid") + convolve2d(fields.curv_yy, virtual_field.okyyfield,
+                                                                                         mode="valid") + 2. * convolve2d(
+        fields.curv_xy, virtual_field.okxyfield, mode="valid")
     A11 = np.real(A11)
 
-    A12 = convolve2d(fields.curv_xx, vfields.okyyfield, mode="valid") + convolve2d(fields.curv_yy, vfields.okxxfield,
-                                                                                   mode="valid") - 2. * convolve2d(
-        fields.curv_xy, vfields.okxyfield, mode="valid")
+    A12 = convolve2d(fields.curv_xx, virtual_field.okyyfield, mode="valid") + convolve2d(fields.curv_yy, virtual_field.okxxfield,
+                                                                                         mode="valid") - 2. * convolve2d(
+        fields.curv_xy, virtual_field.okxyfield, mode="valid")
     A12 = np.real(A12)
 
-    a_u3 = plate.density * plate.thickness * convolve2d(fields.acceleration, vfields.owfield, mode="valid")
+    a_u3 = plate.density * plate.thickness * convolve2d(fields.acceleration, virtual_field.owfield, mode="valid")
     a_u3 = np.real(a_u3)
 
-    U3 = np.sum(vfields.owfield)
+    U3 = np.sum(virtual_field.owfield)
 
     op = (A11 * plate.bend_stiff_11 + A12 * plate.bend_stiff_12 + a_u3) / U3
 
-    return op, 0
+    return op
