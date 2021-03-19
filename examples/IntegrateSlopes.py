89d9cc69f9e772274b220d7476b5c285239a9727
Sindre Nordmark Olufsen <sindre.n.olufsen@ntnu.no>
2021-03-19 10:54:16 +0100

    Better naming

--- examples/IntegrateSlopes.py
+++ examples/IntegrateSlopes.py
@@ -3,28 +3,25 @@ from scipy.io import loadmat
 import numpy as np
 import os
 
-data = loadmat("/home/sindreno/Rene/dataset/slopes.txt") #x,y,frame
+# Integrate slopes to obtain displacement fields
 
-slopes_x = np.moveaxis(data["slope_x"], -1, 0)
-slopes_y = np.moveaxis(data["slope_y"], -1, 0)
+data = loadmat("/home/sindreno/Rene/dataset/slopes.txt")  # x,y,frame
 
-n_frames = slopes_x.shape[0]
+slopes_x = data["slope_x"]
+slopes_y = data["slope_y"]
 
-size = 2.94/1000. #mm (5.88mm)
-#frame_id = 110
+n_frames = slopes_x.shape[-1]
 
-disps = []
+pixel_size = 2.94 / 1000.
 
-for i in np.arange(0,90):
-    print("Integrating frame %i"%i)
-    slope_y = slopes_x[i,:,:90]
-    slope_x = slopes_y[i,:,:90]
+disp_fields = []
 
-    #slope_x = gaussian_filter(slope_x,sigma=2)
-    #slope_y = gaussian_filter(slope_y,sigma=2)
+for i in np.arange(90,130):
+    print("Integrating frame %i" % i)
+    slope_y = slopes_x[:, :90, i]
+    slope_x = slopes_y[:, :90, i]
 
+    disp_field = sparce_integration.int2D(slope_x, slope_y, 0., pixel_size, pixel_size)
+    disp_fields.append(disp_field)
 
-    disp = sparce_integration.int2D(slope_x, slope_y, 0., size, size)
-    disps.append(disp)
-disps = np.array(disps)
-np.save(os.getcwd()+"/disps_shake_sindre",disps)
+np.save(os.getcwd() + "/disp_fields", np.array(disp_fields))
