164326c6193894a5ddaa1b3817893dcec01db61f
Sindre Nordmark Olufsen <sindre.n.olufsen@ntnu.no>
2021-03-22 12:54:18 +0100

    Running implementation

--- recon/slope_integration/sparce_integration.py
+++ recon/slope_integration/sparce_integration.py
@@ -10,6 +10,7 @@ Assumes uniform spacing
 
 import numpy as np
 from numpy import matlib
+import matplotlib.pyplot as plt
 
 from scipy.sparse import csr_matrix
 
@@ -17,95 +18,108 @@ from scipy.sparse import csr_matrix
 ###
 def int2D(igrad_x, igrad_y, iconst, idx, idy):
     # TODO: Make code pythonic
-    [s_x, s_y] = np.shape(igrad_x)
-    diffx = matlib.repmat(idx, s_x, 1)
-    diffy = matlib.repmat(idy, s_y, 1)
+    n_x, n_y = np.shape(igrad_x)
+
+    diffx = np.ones((n_x,1)) * idx
+    diffy = np.ones((n_y,1)) * idy
+
+    indices = np.arange(n_x*n_y,dtype=np.int).reshape((n_x,n_y))
+
+
     # initialize variables
-    b = np.zeros(2 * s_x * s_y)
-    aux_A = np.zeros([2 * s_x * s_y, 6])
+    b = np.zeros(2 * n_x * n_y)
+    aux_A = np.zeros((2 * n_x * n_y, 6))
     current_length = 0
 
     # define forward differences for left hand edge in x-direction
-    aux_ix = 0
-    aux_iy = np.arange(0, s_y, 1)
-    aux_i = aux_ix * (np.transpose(aux_iy) - 1) + aux_iy
 
-    ind_m = matlib.repmat(aux_iy + current_length, 2, 1)
-    ind_n = np.array([aux_i, aux_i + s_y])
+    indy = indices[0,:]
+    inds = indy
 
-    diffopx = matlib.repmat(np.divide(np.array([-1, 1]), diffx[1]), s_y, 1)
+    ind_m = (indy + current_length) * np.ones((2, 1))
+    ind_n = np.array((inds, inds + n_y))
 
-    aux_A[np.arange(0, s_y) + current_length] = np.concatenate((np.transpose(ind_m), np.transpose(ind_n), diffopx),
+    dfdx = (np.array([-1, 1]/diffx[1])) * np.ones((n_y, 1))
+
+    aux_A[indy + current_length] = np.concatenate((np.transpose(ind_m), np.transpose(ind_n), dfdx),
                                                                axis=1)
 
-    b[np.arange(0, s_y, 1) + current_length] = igrad_x[:, 0]
-    current_length = current_length + s_y
+    b[np.arange(0, n_y, 1) + current_length] = igrad_x[0, :]
+    current_length = current_length + n_y
 
     # central differences in x-direction
-    aux_ix, aux_iy = np.meshgrid(np.arange(2, s_y), np.arange(0, (s_x)))
-    aux_i = np.ndarray.flatten(np.transpose(aux_iy)) + np.multiply(np.ndarray.flatten(np.transpose(aux_ix) - 1), s_y)
-    aux_i = np.array([aux_i])
-    end_i = s_y * (s_x - 2)
+    indx, indy = np.meshgrid(np.arange(1, n_x-1), np.arange(0, n_y))
+
+    inds = indices[1:-1,:].flatten()
+
+    inds = np.array([inds])
+    end_i = n_y * (n_x - 2)
     del ind_m, ind_n
     ind_m = matlib.repmat(current_length + np.arange(0, end_i, 1), 2, 1)
-    ind_n = np.concatenate((aux_i - s_y, aux_i + s_y), axis=0)
+    ind_n = np.concatenate((inds - n_y, inds + n_y), axis=0)
 
-    diffopx = np.divide(1, np.ndarray.flatten(diffx[aux_ix - 1, 0]) + np.ndarray.flatten(diffx[aux_ix, 0]))
-    diffopx = np.array([diffopx])
-    diffopx = np.multiply(np.transpose(diffopx), np.array([-1, 1]))
+    dfdx = 1./(diffx[indx - 1, 0].flatten() + diffx[indx, 0].flatten())
+    dfdx = np.array([dfdx])
+    dfdx = np.multiply(np.transpose(dfdx), np.array([-1, 1]))
 
-    aux_A[np.arange(0, end_i) + current_length, :] = np.concatenate((np.transpose(ind_m), np.transpose(ind_n), diffopx),
+    aux_A[np.arange(0, end_i) + current_length, :] = np.concatenate((np.transpose(ind_m), np.transpose(ind_n), dfdx),
                                                                     axis=1)
 
-    aigrad_x = np.ndarray.flatten(igrad_x)
-    aigrad_x = np.array(aigrad_x)
+    b[np.arange(0, end_i) + current_length] = igrad_x.flatten()[inds]
 
-    b[np.arange(0, end_i) + current_length] = aigrad_x[aux_i]
 
     current_length = current_length + end_i
 
     # define forward differences for right hand edge in x-direction
-    aux_ix = s_x
-    aux_iy = np.arange(0, s_y, 1)
-    aux_i = s_y * (np.transpose(aux_ix) - 1) + np.transpose(aux_iy)
+    indx = n_x
+    indy = np.arange(0, n_y)
+    inds = n_y * (np.transpose(indx) - 1) + np.transpose(indy)
+    inds = indices[-1,:]
 
-    ind_m = matlib.repmat(aux_iy + current_length, 2, 1)
-    ind_n = np.array([aux_i - s_y, aux_i])
+    #inds = indx * (indy - 1.) + n_y
 
-    diffopx = matlib.repmat(np.divide(np.array([-1, 1]), diffx[-1]), s_y, 1)
+    ind_m = matlib.repmat(indy + current_length, 2, 1)
+    ind_n = np.array([inds - n_y, inds])
 
-    aux_A[np.arange(0, s_y) + current_length] = np.concatenate((np.transpose(ind_m), np.transpose(ind_n), diffopx),
+    dfdx = matlib.repmat(np.divide(np.array([-1, 1]), diffx[-1]), n_y, 1)
+
+    aux_A[np.arange(0, n_y) + current_length] = np.concatenate((np.transpose(ind_m), np.transpose(ind_n), dfdx),
                                                                axis=1)
 
-    b[np.arange(0, s_y, 1) + current_length] = igrad_x[:, -1]
-    current_length = current_length + s_y
+    b[np.arange(0, n_y, 1) + current_length] = igrad_x[-1, :]
+    current_length = current_length + n_y
 
     ###
     # define forward differences for left hand edge in y-direction
-    aux_ix = np.arange(0, s_x, 1)
-    aux_iy = 0
-    aux_i = s_y * (np.transpose(aux_ix)) + aux_iy
+    indx = np.arange(0, n_x, 1)
+    inds = indices[:,0]
 
-    ind_m = matlib.repmat(aux_ix + current_length, 2, 1)
-    ind_n = np.array([aux_i, aux_i + 1])
+    ind_m = (indx + current_length) * np.ones((2, 1))
+    ind_n = np.array([inds, inds + 1])
+    print("inds should be zero", ind_n)
 
-    diffopy = matlib.repmat(np.divide(np.array([-1, 1]), diffy[1]), s_x, 1)
 
-    aux_A[np.arange(0, s_x) + current_length] = np.concatenate((np.transpose(ind_m), np.transpose(ind_n), diffopy),
+
+    diffopy = matlib.repmat(np.divide(np.array([-1, 1]), diffy[1]), n_x, 1)
+
+    aux_A[np.arange(0, n_x) + current_length] = np.concatenate((np.transpose(ind_m), np.transpose(ind_n), diffopy),
                                                                axis=1)
 
-    b[np.arange(0, s_y, 1) + current_length] = igrad_y[:, 0]
-    current_length = current_length + s_x
+    b[np.arange(0, n_y, 1) + current_length] = igrad_y[:, 0]
+    current_length = current_length + n_x
 
     # central differences in y-direction
-    aux_ix, aux_iy = np.meshgrid(np.arange(0, (s_x)), np.arange(1, s_y - 1))
-    aux_i = np.ndarray.flatten(np.transpose(aux_iy)) + np.multiply((np.ndarray.flatten(np.transpose(aux_ix))), s_y)
-    aux_i = np.array([aux_i])
-    end_i = s_x * (s_y - 2)
+    indx, indy = np.meshgrid(np.arange(0, (n_x)), np.arange(1, n_y - 1))
+    inds = np.ndarray.flatten(np.transpose(indy)) + np.multiply((np.ndarray.flatten(np.transpose(indx))), n_y)
+    inds = indices[:,1:-1].flatten()
+
+    inds = np.array([inds])
+    end_i = n_x * (n_y - 2)
 
     ind_m = matlib.repmat(current_length + np.arange(0, end_i, 1), 2, 1)
-    ind_n = np.concatenate((aux_i - 1, aux_i + 1), axis=0)
-    diffopy = np.divide(1, np.ndarray.flatten(diffy[aux_iy - 1, 0]) + np.ndarray.flatten(diffy[aux_iy, 0]))
+    ind_n = np.concatenate((inds - 1, inds + 1), axis=0)
+
+    diffopy = 1./(diffy[indy - 1, 0].flatten() + (diffy[indy, 0].flatten()))
     diffopy = np.array([diffopy])
     diffopy = np.multiply(np.transpose(diffopy), np.array([-1, 1]))
 
@@ -115,31 +129,30 @@ def int2D(igrad_x, igrad_y, iconst, idx, idy):
     aigrad_y = np.ndarray.flatten(igrad_y)
     aigrad_y = np.array(aigrad_y)
 
-    b[np.arange(0, end_i) + current_length] = aigrad_y[aux_i]
+    b[np.arange(0, end_i) + current_length] = aigrad_y[inds]
 
     current_length = current_length + end_i
 
     # define forward differences for right hand edge in y-direction
-    aux_ix = np.arange(0, s_x, 1)
-    aux_iy = s_y - 1
-    aux_i = s_y * (np.transpose(aux_ix)) + aux_iy
+    indx = np.arange(0, n_x, 1)
+    inds = indices[:,-1]
 
-    ind_m = matlib.repmat(aux_ix + current_length, 2, 1)
-    ind_n = np.array([aux_i - 1, aux_i])
+    ind_m = matlib.repmat(indx + current_length, 2, 1)
+    ind_n = np.array([inds - 1, inds])
 
-    diffopy = matlib.repmat(np.divide(np.array([-1, 1]), diffy[-1]), s_x, 1)
+    diffopy = matlib.repmat(np.divide(np.array([-1, 1]), diffy[-1]), n_x, 1)
 
-    aux_A[np.arange(0, s_x) + current_length] = np.concatenate((np.transpose(ind_m), np.transpose(ind_n), diffopy),
+    aux_A[np.arange(0, n_x) + current_length] = np.concatenate((np.transpose(ind_m), np.transpose(ind_n), diffopy),
                                                                axis=1)
 
-    b[np.arange(0, s_x, 1) + current_length] = igrad_y[:, -1]
+    b[np.arange(0, n_x, 1) + current_length] = igrad_y[:, -1]
 
     ###
     # sparse matrix transformations
-    data = np.ndarray.flatten(aux_A[:, 4:6])
-    indices = np.ndarray.flatten(aux_A[:, 0:2])
-    indptr = np.ndarray.flatten(aux_A[:, 2:4])
-    A = csr_matrix((data, (indices, indptr)), shape=(2 * s_x * s_y, s_x * s_y)).toarray()  #
+    data = aux_A[:, 4:6].flatten()
+    indices = aux_A[:, 0:2].flatten()
+    indptr = aux_A[:, 2:4].flatten()
+    A = csr_matrix((data, (indices, indptr)), shape=(2 * n_x * n_y, n_x * n_y)).toarray()  #
 
     # add integration constant
     b1 = b - A[:, 0] * iconst
@@ -147,14 +160,17 @@ def int2D(igrad_x, igrad_y, iconst, idx, idy):
     # solve linear system
     sa1, sa2 = np.shape(A)
     aux_a1 = np.linalg.lstsq(A[:, 1:sa2], b2)
-    #aux_a1 = np.linalg.solve(A[:, 1:sa2], b2)
 
     aux_a2 = aux_a1[0]
     iconst = np.array([iconst])
     aux_a3 = np.concatenate((iconst, aux_a2))
 
-    oa = np.reshape(aux_a3, (s_y, s_x))
+    oa = np.reshape(aux_a3, (n_y, n_x))
 
     return oa
+
+
+
+
 
 
