88e851ce50913745a3482c80b14f9d96d47e90ce
Sindre Nordmark Olufsen <sindre.n.olufsen@ntnu.no>
2021-03-24 14:40:38 +0100

    Formatting according to PEP8

--- recon/slope_integration/sparce_integration.py
+++ recon/slope_integration/sparce_integration.py
@@ -21,7 +21,7 @@ def int2D(igrad_x, igrad_y, iconst, idx, idy):
     n_x, n_y = np.shape(igrad_x)
 
     indices = np.arange(n_x * n_y, dtype=int).reshape((n_x, n_y))
-    indices_y,indices_x = np.meshgrid(np.arange(n_y),np.arange(n_x))
+    indices_y, indices_x = np.meshgrid(np.arange(n_y), np.arange(n_x))
 
     # initialize variables
     b = np.zeros(2 * n_x * n_y)
@@ -31,16 +31,15 @@ def int2D(igrad_x, igrad_y, iconst, idx, idy):
     current_length = 0
 
     # define forward differences for left hand edge in x-direction
-    flat_inds = indices[0,:]
+    flat_inds = indices[0, :]
     ind_m = (flat_inds + current_length) * np.ones((2, 1))
     ind_n = np.array((flat_inds, flat_inds))
 
-    dfdx = (np.array([-1, 1]) / idx) * np.ones((n_y,1))
+    dfdx = (np.array([-1, 1]) / idx) * np.ones((n_y, 1))
 
-    datas[np.arange(0, np.size(flat_inds)) + current_length,:] = dfdx
-    indptrs[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_n)
-    cov_inds[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_m)
-
+    datas[np.arange(0, np.size(flat_inds)) + current_length, :] = dfdx
+    indptrs[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_n)
+    cov_inds[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_m)
 
     b[np.arange(0, n_y, 1) + current_length] = igrad_x[0, :]
     current_length = current_length + n_y
@@ -55,21 +54,18 @@ def int2D(igrad_x, igrad_y, iconst, idx, idy):
     ind_n = np.concatenate(([indices[0:-2, :].flatten()], [indices[2:, :].flatten()]))
 
     dfdx = 1. / (2. * idx) * np.ones_like(indx).flatten()
-    dfdx = np.array([-1, 1]) * dfdx[:,np.newaxis]
+    dfdx = np.array([-1, 1]) * dfdx[:, np.newaxis]
 
-
-    datas[np.arange(0, np.size(flat_inds)) + current_length,:] = dfdx
-    indptrs[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_n)
-    cov_inds[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_m)
-
-
+    datas[np.arange(0, np.size(flat_inds)) + current_length, :] = dfdx
+    indptrs[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_n)
+    cov_inds[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_m)
 
     b[np.arange(end_i) + current_length] = igrad_x.flatten()[flat_inds]
 
     current_length = current_length + end_i
 
     # define forward differences for right hand edge in x-direction
-    indy = indices_y[-1,:]
+    indy = indices_y[-1, :]
     flat_inds = indices[-1, :]
 
     ind_m = (indy + current_length) * np.ones((2, 1))
@@ -77,11 +73,9 @@ def int2D(igrad_x, igrad_y, iconst, idx, idy):
 
     dfdx = np.array([-1, 1]) / idx * np.ones((n_y, 1))
 
-    datas[np.arange(0, np.size(flat_inds)) + current_length,:] = dfdx
-    indptrs[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_n)
-    cov_inds[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_m)
-
-
+    datas[np.arange(0, np.size(flat_inds)) + current_length, :] = dfdx
+    indptrs[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_n)
+    cov_inds[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_m)
 
     b[indy + current_length] = igrad_x[-1, :]
     current_length = current_length + n_y
@@ -93,13 +87,11 @@ def int2D(igrad_x, igrad_y, iconst, idx, idy):
     ind_m = (indx + current_length) * np.ones((2, 1))
     ind_n = np.array([flat_inds, flat_inds + 1])
 
-    dfdy = (np.array([-1, 1])/ idy) * np.ones((n_x, 1))
+    dfdy = (np.array([-1, 1]) / idy) * np.ones((n_x, 1))
 
-
-    datas[np.arange(0, np.size(flat_inds)) + current_length,:] = dfdy
-    indptrs[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_n)
-    cov_inds[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_m)
-
+    datas[np.arange(0, np.size(flat_inds)) + current_length, :] = dfdy
+    indptrs[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_n)
+    cov_inds[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_m)
 
     b[np.arange(0, n_x, 1) + current_length] = igrad_y[:, 0]
     current_length = current_length + n_x
@@ -111,18 +103,16 @@ def int2D(igrad_x, igrad_y, iconst, idx, idy):
     flat_inds = np.array([flat_inds])
     end_i = n_x * (n_y - 2)
 
-    ind_m = (current_length + np.arange(0, end_i, 1))*np.ones((2, 1))
+    ind_m = (current_length + np.arange(0, end_i, 1)) * np.ones((2, 1))
     ind_n = np.concatenate((flat_inds - 1, flat_inds + 1), axis=0)
 
-    #diffopy = 1. / (diffy[indy - 1, 0].flatten() + (diffy[indy, 0].flatten()))
+    # diffopy = 1. / (diffy[indy - 1, 0].flatten() + (diffy[indy, 0].flatten()))
     dfdy = 1. / (2. * idy) * np.ones_like(indy).flatten()
-    dfdy = dfdy.transpose()[:,np.newaxis] * np.array([-1, 1])
+    dfdy = dfdy.transpose()[:, np.newaxis] * np.array([-1, 1])
 
-
-    datas[np.arange(0, np.size(flat_inds)) + current_length,:] = dfdy
-    indptrs[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_n)
-    cov_inds[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_m)
-
+    datas[np.arange(0, np.size(flat_inds)) + current_length, :] = dfdy
+    indptrs[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_n)
+    cov_inds[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_m)
 
     aigrad_y = igrad_y.flatten()
 
@@ -135,15 +125,12 @@ def int2D(igrad_x, igrad_y, iconst, idx, idy):
     flat_inds = indices[:, -1]
     ind_m = (indx + current_length) * np.ones((2, 1))
     ind_n = np.array([flat_inds - 1, flat_inds])
-    dfdy = (np.array([-1, 1])/ idy) * np.ones((n_x, 1))
+    dfdy = (np.array([-1, 1]) / idy) * np.ones((n_x, 1))
 
-
-    datas[np.arange(0, np.size(flat_inds)) + current_length,:] = dfdy
-    indptrs[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_n)
-    cov_inds[np.arange(0, np.size(flat_inds)) + current_length,:] = np.transpose(ind_m)
+    datas[np.arange(0, np.size(flat_inds)) + current_length, :] = dfdy
+    indptrs[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_n)
+    cov_inds[np.arange(0, np.size(flat_inds)) + current_length, :] = np.transpose(ind_m)
     b[np.arange(0, n_x, 1) + current_length] = igrad_y[:, -1]
-
-
 
     # sparse matrix transformations
     A = csr_matrix((datas.flatten(), (cov_inds.flatten(), indptrs.flatten())), shape=(2 * n_x * n_y, n_x * n_y))  #
