2eb0bd8ae7bc92a00ba927c885d67ee25f0eedf7
Sindre Nordmark Olufsen <sindre.n.olufsen@ntnu.no>
2021-03-25 14:06:58 +0100

    Updated for better noise rejection

--- examples/reconstuctExperimentalCropping.py
+++ examples/reconstuctExperimentalCropping.py
@@ -8,11 +8,21 @@ def read_exp_press_data():
     start = 25500  # + 38
     end = 26200
     data = np.genfromtxt("./experimentalPressures/trans_open_1.txt", skip_header=20)
+    #data = np.genfromtxt("./experimentalPressures/trans_half_1.txt", skip_header=20)
 
     time = data[start:end, 0] * 1.e-3
     time = time - time[0]
-    press = data[start:end, 8] / 10.
-    return press - press[0], time
+    press = data[start:end, 6] / 10.
+    return press-press[0], time
+
+
+def mean_of_array_frame(array):
+    top = np.mean(array[0, :])
+    btm = np.mean(array[-1, :])
+    lft = np.mean(array[:, 0])
+    right = np.mean(array[:, -1])
+    # return np.mean([top,btm,lft,right])
+    return np.mean([btm])
 
 
 def crop_and_integrate_exp_data(crop_factor):
@@ -21,9 +31,15 @@ def crop_and_integrate_exp_data(crop_factor):
 
     # Integrate slopes to obtain displacement fields
     data = loadmat("/home/sindreno/Downloads/Rene/slopes_5_0_set1_full3.mat")
+    #data = loadmat("/home/sindreno/Downloads/Rene/slopes_5_0_set1_half1.mat")
 
     slopes_x = data["slope_x"]
     slopes_y = data["slope_y"]
+
+    slopes_x = slopes_x - np.mean(slopes_x[:, :, 0])
+    slopes_y = slopes_y - np.mean(slopes_y[:, :, 0])
+
+
 
     pixel_size = 2.94 / 1000.
 
@@ -41,24 +57,28 @@ def crop_and_integrate_exp_data(crop_factor):
             slope_y = slope_y[crop_factor:-crop_factor, crop_factor:-crop_factor]
             slope_x = slope_x[crop_factor:-crop_factor, crop_factor:-crop_factor]
         elif crop_factor < 0:
-            slope_x = np.pad(slope_x, pad_width=-crop_factor, mode="edge")
-            slope_y = np.pad(slope_y, pad_width=-crop_factor, mode="edge")
+            slope_x = np.pad(slope_x, pad_width=(-crop_factor,-crop_factor), mode="edge")
+            slope_y = np.pad(slope_y, pad_width=(-crop_factor,-crop_factor), mode="edge")
 
         disp_field = sparce_integration.int2D(slope_x, slope_y, 0., pixel_size, pixel_size)
+
+        border = mean_of_array_frame(disp_field)
+        disp_field = disp_field - border
+
         disp_fields.append(disp_field)
 
     return np.array(disp_fields)
 
 
 # Parameters for parameter study
-crop_pixels = range(-2, 6, 2)
+# crop_pixels = range(-2, 6, 2)
 crop_pixels = [-2]
 
 # plate and model parameters
 mat_E = 210.e9  # Young's modulus [Pa]
 mat_nu = 0.3  # Poisson's ratio []
-plate_thick = 5e-3
-rho = 7850.
+plate_thick = 4.95e-3
+rho = 7934.
 
 plate = recon.calculate_plate_stiffness(mat_E, mat_nu, rho, plate_thick)
 
@@ -70,7 +90,6 @@ sampling_rate = 75000.
 for crop_pixel in crop_pixels:
 
     raw_disp_field = -crop_and_integrate_exp_data(crop_pixel)
-
     # Results are stored in these lists
     times = []
     presses = []
@@ -92,18 +111,19 @@ for crop_pixel in crop_pixels:
 
     # Plot the results
     if crop_pixel >= 0:
-        plt.plot(np.array(times[:]) - 0.00007, presses[:, center - 2, center + 1], '-o',
+        plt.plot(np.array(times[:]) - 0.00007, presses[:, center,  center], '-o',
                  label="Cropped by %i pixels" % crop_pixel)
     else:
-        plt.plot(np.array(times[:]) - 0.00007, presses[:, center - 2, center + 1], '-o',
+        plt.plot(np.array(times[:]) - 0.00007, presses[:, center, center], '-o',
                  label="Padded by %i pixels" % (-crop_pixel))
 
 real_press, real_time = read_exp_press_data()
+
 plt.plot(real_time, real_press * 1.e6, '--', label="Transducer")
 plt.plot(real_time, gaussian_filter(real_press, sigma=2. * 500. / 75.) * 1.e6, '--',
          label="We should get this curve for sigma=2")
 
-plt.xlim(left=0.000, right=0.0006)
+plt.xlim(left=0.000, right=0.0009)
 plt.ylim(top=80000, bottom=-15000)
 plt.xlabel("Time [Sec]")
 plt.ylabel("Pressure [Pa]")
