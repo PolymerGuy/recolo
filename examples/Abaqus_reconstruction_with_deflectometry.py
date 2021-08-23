e9e3ae811ef617044da6a95d5091025fb492f694
Sindre Nordmark Olufsen <sindre.n.olufsen@ntnu.no>
2021-08-23 09:48:08 +0200

    Added full example with deflectometry

--- examples/Abaqus_reconstruction_with_deflectometry.py
+++ examples/Abaqus_reconstruction_with_deflectometry.py
@@ -2,7 +2,11 @@ import recon
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.style.use('science')
+# Minimal example of pressure load reconstruction based on input from Abaqus. The deflection field is used to
+# generate images used for deflectometry. This operation necessitates the images to be upscaled. Minor deviations
+# from the correct pressure field occurs due to the use of central differences to determine the acceleration fields,
+# introduction significant errors when the temporal resolution is as bad as here.
+
 
 # plate and model parameters
 mat_E = 210.e9  # Young's modulus [Pa]
@@ -11,18 +15,20 @@ density = 7700
 plate_thick = 5e-3
 plate = recon.calculate_plate_stiffness(mat_E, mat_nu, density, plate_thick)
 
-deflecto_upscale = 4
+# Reconstruction settings
+win_size = 6
+
+# Deflectometry settings
+run_deflectometry = True
+deflecto_upscale = 8
 mirror_grid_dist = 500.
-win_size = 10
 grid_pitch = 5.  # pixels
 
 # Load Abaqus data
-abq_sim_fields = recon.load_abaqus_rpts("/home/sindreno/Rene/testfolder/fields/")
-pixel_size = abq_sim_fields.pixel_size_x# / deflecto_upscale
+abq_sim_fields = recon.load_abaqus_rpts("./AbaqusExampleData/")
 
-
-if False:
-# Deflectometry
+# The deflectometry return the slopes of the plate which has to be integrated in order to determine the deflection
+if run_deflectometry:
     slopes_x = []
     slopes_y = []
     undeformed_grid = recon.artificial_grid_deformation.deform_grid_from_deflection(abq_sim_fields.disp_fields[0, :, :],
@@ -43,31 +49,34 @@ if False:
 
     slopes_x = np.array(slopes_x)
     slopes_y = np.array(slopes_y)
+    pixel_size = abq_sim_fields.pixel_size_x / deflecto_upscale
+else:
+    pixel_size = abq_sim_fields.pixel_size_x
+    slopes_x, slopes_y = np.gradient(abq_sim_fields.disp_fields, pixel_size, axis=(1, 2))
 
-slopes_x,slopes_y = np.gradient(abq_sim_fields.disp_fields,pixel_size,axis=(1,2))
-# Integrate slopes from deflectometry to get deflection fields
+# Integrate slopes to get deflection fields
 disp_fields = recon.slope_integration.disp_from_slopes(slopes_x, slopes_y, pixel_size,
                                                        zero_at="bottom corners", zero_at_size=1,
                                                        extrapolate_edge=0, filter_sigma=0, downsample=1)
 
-
 # Kinematic fields from deflection field
-sampling_rate = 1. / (abq_sim_fields.times[1] - abq_sim_fields.times[0])
-fields = recon.kinematic_fields_from_deflections(disp_fields, pixel_size, sampling_rate)
+kin_fields = recon.kinematic_fields_from_deflections(disp_fields, pixel_size,
+                                                     abq_sim_fields.sampling_rate)
 
 # Reconstruct pressure using the virtual fields method
 virtual_field = recon.virtual_fields.Hermite16(win_size, pixel_size)
-pressure_fields = [recon.solver.pressure_elastic_thin_plate(field, plate, virtual_field) for field in fields]
+pressure_fields = np.array(
+    [recon.solver_VFM.pressure_elastic_thin_plate(field, plate, virtual_field) for field in kin_fields])
 
-pressure_fields = np.array(pressure_fields)
-center = int(pressure_fields.shape[1] / 2)
 # Plot the results
 # Correct
-times = np.array([0.0,0.00005,0.000051,0.0003,0.001])*1000
-pressures = np.array([0.0,0.0,1.0,0.0,0.0]) * 1e5
-plt.plot(times,pressures,'--')
+times = np.array([0.0, 0.00005, 0.00010, 0.0003, 0.001]) * 1000
+pressures = np.array([0.0, 0.0, 1.0, 0.0, 0.0]) * 1e5
+plt.plot(times, pressures, '-', label="Correct pressure")
 
-plt.plot(abq_sim_fields.times * 1000., pressure_fields[:, center, center])
+# Reconstructed
+center = int(pressure_fields.shape[1] / 2)
+plt.plot(abq_sim_fields.times * 1000., pressure_fields[:, center, center], "-o", label="Reconstructed pressure")
 
 plt.xlim(left=0.000, right=0.3)
 plt.ylim(top=110000, bottom=-15)
