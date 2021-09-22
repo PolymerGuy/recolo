2dc7178a2682f3c19bd5feecb3858c8cdd767470
Sindre Nordmark Olufsen <sindre.n.olufsen@ntnu.no>
2021-09-22 09:38:44 +0200

    PEP8 formatting

--- recolo/tests/test_AbaqusRPTImport.py
+++ recolo/tests/test_AbaqusRPTImport.py
@@ -1,14 +1,14 @@
 from unittest import TestCase
-from  recolo.data_structures import load_abaqus_rpts
+from recolo.data_structures import load_abaqus_rpts
 import pathlib
 import os
+
 cwd = pathlib.Path(__file__).parent.resolve()
 
 
 class TestReadAbaqusField(TestCase):
     def setUp(self):
-        path_to_rpts = os.path.join(cwd,"ExampleAbaqusRPT/")
-
+        path_to_rpts = os.path.join(cwd, "ExampleAbaqusRPT/")
 
         self.abaqus_fields = load_abaqus_rpts(path_to_rpts)
 
@@ -17,9 +17,8 @@ class TestReadAbaqusField(TestCase):
         shape_accelerations = self.abaqus_fields.accel_fields.shape
         shape_time = self.abaqus_fields.times.shape
 
-
         if shape_deflection != shape_accelerations:
             self.fail("The deflection and acceleration field has different shapes")
         if shape_deflection[0] != shape_time[0]:
-            self.fail(("The time axis is not of the same length (%i) as the number of frames (%i)"%(shape_deflection[0],shape_time[0])))
-
+            self.fail(("The time axis is not of the same length (%i) as the number of frames (%i)" % (
+            shape_deflection[0], shape_time[0])))
